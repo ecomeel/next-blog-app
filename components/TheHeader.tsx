b4258fe8d3b8002fda978e0eef9b6327223dd318
ecomeel <ecomeel@gmail.com>
2024-01-11 19:12:40 +0300

    first commit

---
+++ components/TheHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+export const TheHeader = () => {
+  return (
+    <header className="container">
+      <Link href="/">Home</Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/about">About</Link>
+    </header>
+  );
+};
+
