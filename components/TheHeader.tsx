16b019c0cfb70d0a63c0c9c6fb0969fb79406f17
ecomeel <ecomeel@gmail.com>
2024-01-13 22:55:22 +0300

    active links

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
 
+import Navigation from "./Navigation";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
+
 export const TheHeader: React.FC = () => {
   return (
     <header className="container">
-      <Link href="/">Home</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/about">About</Link>
+      <Navigation navLinks={navItems} />
     </header>
   );
 };
-
